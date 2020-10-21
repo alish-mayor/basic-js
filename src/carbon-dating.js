dd8a41ab801c51ce55d740f01b7e610ecd7d3bcc
alish-mayor <alish22061999@gmail.com>
2020-10-21 17:24:24 +0600

    feat: what-season task solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,5 +4,4 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,55 @@
+const { prototype } = require("../extensions/custom-error");
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {  
+    
+    if (date === undefined){
+      return 'Unable to determine the time of year!'
+    }
+  
+    if(Object.prototype.toString.call(date) != "[object Date]"){
+      throw new Error("Can't to use methods of invalid object")
+    }
+    
+      let month = date.getMonth()
+      
+      switch(month){
+        case 11:
+        return "winter"
+        break
+        case 0:
+        return "winter"
+        break
+        case 1:
+        return "winter"
+        break
+        case 2:
+        return "spring"
+        break
+        case 3:
+        return "spring"
+        break
+        case 4:
+        return "spring"
+        break
+        case 5:
+        return "summer"
+        break
+        case 6:
+        return "summer"
+        break
+        case 7:
+        return "summer"
+        break
+        case 8:
+        return "autumn"
+        break
+        case 9:
+        return "autumn"
+        break
+        case 10:
+        return "autumn"
+        break
+      }
+    
 };
