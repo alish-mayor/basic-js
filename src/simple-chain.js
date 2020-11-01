5f0a051e9d134283eea697e724c7dc48d3081c90
alish-mayor <alish22061999@gmail.com>
2020-11-01 17:23:00 +0600

    feat: simple-chain solved

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+  
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.chain.length || typeof position != "number"){
+     this.chain = [];
+     throw new Error("THROWN");
+    } else {
+      this.chain.splice(position - 1, 1)
+      return this;
+    }
+    
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let completedChain = this.chain.join('~~');
+    this.chain = [];
+    return completedChain;
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  if(!(Array.isArray(arr))){
+    throw new CustomError("invalid argument");
+  }
+
+  if(arr.length === 0){
+    return arr;
+  }
+
+  arr.forEach(element =>{
+    if (typeof element === "string"){
+      switch(element){
+        case "--discard-next":
+          return discardNext(arr);
+        case "--discard-prev":
+          return discardPrev(arr);
+        case "--double-next":
+          return doubleNext(arr);
+        case "--double-prev":
+          return doublePrev(arr);
+      }
+    } else {
+      return arr;
+    }
+  });
+
+  
+
 };
+
+
+
+
+function checkArrAndDeleteOther(arr){
+  
+}
